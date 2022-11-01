f2171e2f34bf7cecdd9869a63077d96d00b78154
Valentin Dubuisson <val.dub03@gmail.com>
2022-11-01 13:44:17 +0100

    Init

---
+++ apps/loup-garou-mj/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'loup-garou_mj',
+  webDir: '../../dist/apps/loup-garou-mj',
+  bundledWebRuntime: false,
+};
+
+export default config;
