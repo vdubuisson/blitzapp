83e034cacbf3781240078f44559c3558d8f06c4a
Valentin Dubuisson <val.dub03@gmail.com>
2022-12-10 17:13:50 +0100

    relative image path

--- apps/loup-garou-mj/src/app/core/pipes/player-role-image/player-role-image.pipe.spec.ts
+++ apps/loup-garou-mj/src/app/core/pipes/player-role-image/player-role-image.pipe.spec.ts
@@ -13,41 +13,41 @@ describe('PlayerRoleImagePipe', () => {
 
   it('should return image path for VILLAGEOIS', () => {
     const path = pipe.transform(PlayerRoleEnum.VILLAGEOIS);
-    expect(path).toEqual('/assets/images/roles/villageois.webp');
+    expect(path).toEqual('assets/images/roles/villageois.webp');
   });
 
   it('should return image path for LOUP_GAROU', () => {
     const path = pipe.transform(PlayerRoleEnum.LOUP_GAROU);
-    expect(path).toEqual('/assets/images/roles/loup_garou.webp');
+    expect(path).toEqual('assets/images/roles/loup_garou.webp');
   });
 
   it('should return image path for CHASSEUR', () => {
     const path = pipe.transform(PlayerRoleEnum.CHASSEUR);
-    expect(path).toEqual('/assets/images/roles/chasseur.webp');
+    expect(path).toEqual('assets/images/roles/chasseur.webp');
   });
 
   it('should return image path for CUPIDON', () => {
     const path = pipe.transform(PlayerRoleEnum.CUPIDON);
-    expect(path).toEqual('/assets/images/roles/cupidon.webp');
+    expect(path).toEqual('assets/images/roles/cupidon.webp');
   });
 
   it('should return image path for PETITE_FILLE', () => {
     const path = pipe.transform(PlayerRoleEnum.PETITE_FILLE);
-    expect(path).toEqual('/assets/images/roles/petite_fille.webp');
+    expect(path).toEqual('assets/images/roles/petite_fille.webp');
   });
 
   it('should return image path for SORCIERE', () => {
     const path = pipe.transform(PlayerRoleEnum.SORCIERE);
-    expect(path).toEqual('/assets/images/roles/sorciere.webp');
+    expect(path).toEqual('assets/images/roles/sorciere.webp');
   });
 
   it('should return image path for VOLEUR', () => {
     const path = pipe.transform(PlayerRoleEnum.VOLEUR);
-    expect(path).toEqual('/assets/images/roles/voleur.webp');
+    expect(path).toEqual('assets/images/roles/voleur.webp');
   });
 
   it('should return image path for VOYANTE', () => {
     const path = pipe.transform(PlayerRoleEnum.VOYANTE);
-    expect(path).toEqual('/assets/images/roles/voyante.webp');
+    expect(path).toEqual('assets/images/roles/voyante.webp');
   });
 });
