2766e16f61b2d4c022725063f83afd5364437dca
Valentin Dubuisson <val.dub03@gmail.com>
2024-07-14 17:07:03 +0200

    lint-staged

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.blitzapp.lgmj',
+  appName: 'loup-garou-mj',
+  webDir: 'dist/browser',
+};
+
+export default config;
